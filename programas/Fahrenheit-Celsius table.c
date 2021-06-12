33246e55fe0ec4c01c12f838fc42639e2d43639c
Lups <83521622+MrLups@users.noreply.github.com>
2021-06-12 03:52:15 -0300

    Update Fahrenheit-Celsius table.c

--- programas/Fahrenheit-Celsius table.c
+++ programas/Fahrenheit-Celsius table.c
@@ -14,9 +14,11 @@ upper = 300; //upper limit
 step = 20; //step size
 
 fahr = lower;
+
+printf("FAHRENHEIT TO CELSIUS TABLE\n\nFAHR\tCELSIUS");
 while (fahr <= upper){
         celsius = (5.0/9.0)*(fahr-32.0);
-        printf("%3.0f\t%6.1f\n",fahr,celsius);
+        printf("\n%3.0f\t%6.1f\n",fahr,celsius);
         fahr = fahr + step;
         }
 }

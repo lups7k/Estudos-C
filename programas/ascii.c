5a9386da97a29415d27b9bb2cc5fdffac2948f53
Lups <83521622+MrLups@users.noreply.github.com>
2021-06-17 18:09:18 -0300

    Create ascii.c

---
+++ programas/ascii.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter a character : ");
+    scanf("%c" , &c);
+    printf("\n\nASCII value of %c = %d",c,c);
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
